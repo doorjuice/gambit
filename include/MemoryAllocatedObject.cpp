57d8b29bd78a276dbc9e75f8bdff6d34d682f423
David Giasson <david.giasson@outlook.com>
2015-12-22 07:37:04 -0500

    Further refactoring of mark_array into MemoryAllocatedObject and subclasses.

--- include/MemoryAllocatedObject.cpp
+++ include/MemoryAllocatedObject.cpp
@@ -1,21 +1,48 @@
 #include "MemoryAllocatedObject.h"
 
 MemoryAllocatedObject::MemoryAllocatedObject(___WORD obj) {
+    //assert(___MEM_ALLOCATED(obj));
     body = ___UNTAG(obj) + ___BODY_OFS;
     head = body[-1];
     subtype = ___HD_SUBTYPE(head);
-    head_typ = ___HD_TYP(head);
 }
 
-___SIZE_TS MemoryAllocatedObject::getSize() {
+
+// ----------------
+
+MovableObject::MovableObject(___WORD obj)
+    : MemoryAllocatedObject(obj) {
+    ;
+}
+
+___SIZE_TS MovableObject::getSize() {
     return ___HD_WORDS(head);
 }
 
-___WORD *MemoryAllocatedObject::move(___WORD *dest) {
+___WORD* MovableObject::move(___WORD* dest) {
     *dest++ = head;
     body[-1] = ___TAG((dest - ___BODY_OFS), ___FORW);
     ___SIZE_TS words = getSize();
     while (words-- > 0)
         *dest++ = *body++;
     return dest;
+}
+
+
+// ----------------
+
+StillObject::StillObject(___WORD obj)
+    : MemoryAllocatedObject(obj) {
+    ;
+}
+
+bool StillObject::isMarked() {
+    return body[___STILL_MARK_OFS - ___STILL_BODY_OFS] != -1;
+}
+
+___WORD StillObject::mark(___WORD scanList) {
+    //assert(!isMarked()); TODO
+    body[___STILL_MARK_OFS - ___STILL_BODY_OFS] = scanList;
+    scanList = ___CAST(___WORD,body - ___STILL_BODY_OFS);
+    return scanList;
 }
--- include/MemoryAllocatedObject.h
+++ include/MemoryAllocatedObject.h
@@ -1,18 +1,57 @@
 #ifndef MemoryAllocatedObject_H
 #define MemoryAllocatedObject_H
 
+/* Constants related to representation of permanent and still objects: */
+
+#ifdef ___USE_HANDLES
+#define ___PERM_HAND_OFS 0
+#define ___PERM_BODY_OFS 2
+#else
+#define ___PERM_HAND_OFS ___PERM_BODY_OFS
+#define ___PERM_BODY_OFS 1
+#endif
+
+#define ___STILL_LINK_OFS 0
+#define ___STILL_REFCOUNT_OFS 1
+#define ___STILL_LENGTH_OFS 2
+#define ___STILL_MARK_OFS 3
+#ifdef ___USE_HANDLES
+#define ___STILL_HAND_OFS 4
+#define ___STILL_BODY_OFS 6
+#else
+#define ___STILL_HAND_OFS ___STILL_BODY_OFS
+#define ___STILL_BODY_OFS (5+1)/************/
+#endif
+
+/*---------------------------------------------------------------------------*/
+
 class MemoryAllocatedObject {
   
   	public:
 	
     ___WORD *body;
     ___WORD head;
-    int head_typ;
     int subtype;
 
     MemoryAllocatedObject(___WORD obj);
+};
+
+class MovableObject : public MemoryAllocatedObject {
+    public:
+
+    MovableObject(___WORD obj);
+    
     ___SIZE_TS getSize();
     ___WORD *move(___WORD *dest);
+};
+
+class StillObject : public MemoryAllocatedObject {
+    public:
+
+    StillObject(___WORD obj);
+    
+    bool isMarked();
+    ___WORD mark(___WORD scanList);
 };
 
 #endif
