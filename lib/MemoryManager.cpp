5624e907d6e4955d70c0abe9488a168a5459f5a9
David Giasson <david.giasson@outlook.com>
2016-01-25 11:48:44 -0500

    Refactored MemoryManager.

--- lib/MemoryManager.cpp
+++ lib/MemoryManager.cpp
@@ -1,6 +1,10 @@
 #include "MemoryManager.h"
 
-void MemoryManager::next_heap_msection(___processor_state ___ps)
+MemoryManager::MemoryManager(___processor_state ps)
+: ___ps(ps) 
+{ }
+
+void MemoryManager::next_heap_msection()
 {
   if (heap_msection != 0)
     {
@@ -8,13 +12,13 @@ void MemoryManager::next_heap_msection(___processor_state ___ps)
       heap_msection->alloc = alloc_heap_ptr;
     }
 
-  heap_msection = next_msection (___ps, stack_msection);
-  alloc_heap_start = start_of_tospace (___VMSTATE_FROM_PSTATE(___ps), heap_msection);
+  heap_msection = next_msection(stack_msection);
+  alloc_heap_start = start_of_tospace(heap_msection);
   alloc_heap_limit = alloc_heap_start + (___MSECTION_SIZE>>1);
   alloc_heap_ptr = alloc_heap_start;
 }
 
-___msection* MemoryManager::next_msection(___processor_state ___ps, ___msection *ms)
+___msection* MemoryManager::next_msection(___msection *ms)
 {
   ___msection *result;
 
@@ -26,7 +30,7 @@ ___msection* MemoryManager::next_msection(___processor_state ___ps, ___msection 
   if (result == 0)
     {
       if (stack_msection == heap_msection)
-        fatal_heap_overflow ();
+        fatal_heap_overflow();
       result = ms;
     }
   else
@@ -38,24 +42,16 @@ ___msection* MemoryManager::next_msection(___processor_state ___ps, ___msection 
   return result;
 }
 
-___WORD* MemoryManager::start_of_tospace(___virtual_machine_state ___vms, ___msection *s)
+___WORD* MemoryManager::start_of_tospace(___msection *s)
 {
-#undef ___VMSTATE_MEM
-#define ___VMSTATE_MEM(var) ___vms->mem.var
-
   if (tospace_at_top)
     return s->base + (___MSECTION_SIZE>>1);
   else
     return s->base;
-
-#undef ___VMSTATE_MEM
-#define ___VMSTATE_MEM(var) ___VMSTATE_FROM_PSTATE(___ps)->mem.var
 }
 
 void MemoryManager::fatal_heap_overflow()
 {
-  char *msgs[2];
-  msgs[0] = "Heap overflow";
-  msgs[1] = 0;
-  ___fatal_error (msgs);
+  char* msgs[] = {"Heap overflow", NULL};
+  ___fatal_error(msgs);
 }
--- lib/MemoryManager.h
+++ lib/MemoryManager.h
@@ -9,14 +9,18 @@
 
 extern void ___fatal_error(char** msgs);
 
-class MemoryManager {
-    
-    public:
-    void next_heap_msection(___processor_state ___ps);
+class MemoryManager : private ___msections {
     
     private:
-    ___msection *next_msection(___processor_state ___ps, ___msection *ms);
-    ___WORD *start_of_tospace(___virtual_machine_state ___vms, ___msection *s);
+    const ___processor_state ___ps;
+    
+    public:
+    MemoryManager(___processor_state ps);
+    void next_heap_msection();
+    
+    private:
+    ___msection *next_msection(___msection *ms);
+    ___WORD *start_of_tospace(___msection *s);
     void fatal_heap_overflow();
 };
 
